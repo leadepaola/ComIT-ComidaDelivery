79cabf7410e5f741be3b58fd1c38924e2ed0ce0d
Arkanyan <agustinrafa1995@hotmail.com>
2018-03-05 19:52:16 -0300

    Primera version

---
+++ ProyectoPedidos/ProyectoPedidos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoPedidos.Startup))]
+namespace ProyectoPedidos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
