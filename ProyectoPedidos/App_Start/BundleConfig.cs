9825d1f90418972d64b8abbb1df8a55e7ce01137
Leandro <leandrogdepaola@gmail.com>
2018-03-16 06:20:09 -0300

    Correcciones Barra lateral

--- ProyectoPedidos/App_Start/BundleConfig.cs
+++ ProyectoPedidos/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace ProyectoPedidos
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/estilos.css"));
         }
     }
 }
